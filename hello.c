e87ecde6349393870139a16a5e8720e697a0a5ee
Elijah Lundstrom <Lunchroomjr@yahoo.com>
2019-08-29 12:10:41 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Elijah Lundstrom
+ * Date: 2019/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Eli Lundstrom\n");
+
+  return 0;
+}
